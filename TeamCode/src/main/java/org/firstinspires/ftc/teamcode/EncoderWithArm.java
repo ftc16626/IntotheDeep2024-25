370ef393a69bdbee7ceeaaa2bde95b972185e7ed
Dandles54 <royowenthomas@gmail.com>
2024-10-17 15:47:35 -0500

    sd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderWithArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderWithArm.java
@@ -60,7 +60,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Red Right", group="Shame")
+@Autonomous(name="Red Right With Arm", group="Shame")
 
 public class EncoderWithArm extends LinearOpMode {
 
