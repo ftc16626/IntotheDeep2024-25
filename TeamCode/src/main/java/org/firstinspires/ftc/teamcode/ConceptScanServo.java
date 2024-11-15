b2ad3a7c2b742a0feb180c18a5add4ec1dd6f5d3
Coderchris842 <christopher_coder@outlook.com>
2024-11-15 10:23:26 -0600

    Update ConceptScanServo.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptScanServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptScanServo.java
@@ -33,16 +33,17 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
 
 /*
  * This OpMode scans a single servo back and forward until Stop is pressed.
  * The code is structured as a LinearOpMode
- * INCREMENT sets how much to increase/decrease the servo position each cycle
+ * INCREMENT sets how much to increase/decrease the servo power each cycle
  * CYCLE_MS sets the update period.
  *
  * This code assumes a Servo configured with the name "left_hand" as is found on a Robot.
  *
- * NOTE: When any servo position is set, ALL attached servos are activated, so ensure that any other
+ * NOTE: When any servo power is set, ALL attached servos are activated, so ensure that any other
  * connected servos are able to move freely before running this test.
  *
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
@@ -54,12 +55,12 @@ public class ConceptScanServo extends LinearOpMode {
 
     static final double INCREMENT   = 0.01;     // amount to slew servo each CYCLE_MS cycle
     static final int    CYCLE_MS    =   50;     // period of each cycle
-    static final double MAX_POS     =  1.0;     // Maximum rotational position
-    static final double MIN_POS     =  0.0;     // Minimum rotational position
+    static final double MAX_POS     =  1.0;     // Maximum rotational power
+    static final double MIN_POS     =  0.0;     // Minimum rotational power
 
     // Define class members
-    Servo   servo;
-    double  position = (MAX_POS - MIN_POS) / 2; // Start at halfway position
+    CRServo   servo;
+    double  power = (MAX_POS - MIN_POS) / 2; // Start at halfway power
     boolean rampUp = true;
 
 
@@ -68,44 +69,45 @@ public class ConceptScanServo extends LinearOpMode {
 
         // Connect to servo (Assume Robot Left Hand)
         // Change the text in quotes to match any servo name on your robot.
-        servo = hardwareMap.get(Servo.class, "left_hand");
+        servo = hardwareMap.get(CRServo.class, "left_hand");
 
         // Wait for the start button
         telemetry.addData(">", "Press Start to scan Servo." );
         telemetry.update();
         waitForStart();
-
+        power = 0.5;
 
         // Scan servo till stop pressed.
         while(opModeIsActive()){
-
+            /*
             // slew the servo, according to the rampUp (direction) variable.
             if (rampUp) {
                 // Keep stepping up until we hit the max value.
-                position += INCREMENT ;
-                if (position >= MAX_POS ) {
-                    position = MAX_POS;
+                power += INCREMENT ;
+                if (power >= MAX_POS ) {
+                    power = MAX_POS;
                     rampUp = !rampUp;   // Switch ramp direction
                 }
             }
             else {
                 // Keep stepping down until we hit the min value.
-                position -= INCREMENT ;
-                if (position <= MIN_POS ) {
-                    position = MIN_POS;
+                power -= INCREMENT ;
+                if (power <= MIN_POS ) {
+                    power = MIN_POS;
                     rampUp = !rampUp;  // Switch ramp direction
                 }
             }
-
+*/
             // Display the current value
-            telemetry.addData("Servo Position", "%5.2f", position);
+            telemetry.addData("Servo power", "%5.2f", power);
             telemetry.addData(">", "Press Stop to end test." );
             telemetry.update();
 
-            // Set the servo to the new position and pause;
-            servo.setPosition(position);
+            // Set the servo to the new power and pause;
+            servo.setPower(power);
             sleep(CYCLE_MS);
             idle();
+            power = 1.0;
         }
 
         // Signal done;
